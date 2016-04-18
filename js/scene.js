bebb25e942d2a087da5c878b27e63bfab10f6798
KnightNiwrem <KnightNiwrem@users.noreply.github.com>
2016-04-18 17:25:24 +0800

    Update scene.js

--- js/scene.js
+++ js/scene.js
@@ -18,8 +18,8 @@
 		
 		// Object Type, smoothness, (r, g, b) colors, (spec, diff, amb, opac), (x, y, z) position, radius, 
         ObjTyp.SPHERE,   10,        0.05, 0.25, 0.9,  1.0, 0.4, 0.5, 0.7,      15, 16, 8.5,        1,    
-        ObjTyp.SPHERE,   10,        1.0, 0.05, 0.05,  0.7, 0.4, 0.6, 1.0,      15, 14, 8.5,        0.4,               
-		ObjTyp.SPHERE,   10,        1.0, 1.0, 0.1,    0.7, 0.4, 0.6, 1.0,      15, 16, 10.5,       0.2           
+        ObjTyp.SPHERE,   10,        1.0, 0.05, 0.05,  0.7, 0.4, 0.6, 1.0,      15, 14, 8.5,        0.6,               
+		ObjTyp.SPHERE,   10,        1.0, 1.0, 0.1,    0.7, 0.4, 0.6, 1.0,      15, 16, 10.5,       0.4           
     ];
    
    var number_of_objects = objects[0];
